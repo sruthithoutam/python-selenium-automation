6b3be845c1d13874e0466570abe48b4eae21747e
Sruthi Thoutam <sruthi.thoutam@gmail.com>
2024-12-19 16:41:13 -0600

    Home Work 2 _ Updated Code

--- HW_2_Target_Signin_Page.py
+++ HW_2_Target_Signin_Page.py
@@ -12,12 +12,26 @@ driver_path = ChromeDriverManager().install()
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
+driver.implicitly_wait(5)
 
 # open the url
 driver.get(" https://www.target.com/ ")
 driver.find_element(By.ID,"account-sign-in").click()
-driver.find_element(By.XPATH,"//button[@data-test='accountNav-signIn']").click()
-driver.find_elements(By.NAME,"Sign into your Target account")
-driver.find_elements(By.NAME,"Sign in with password")
+driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
+#verify the sign in target page
+expected='Sign into your Target account'
+actual=driver.find_element(By.XPATH,"//h1/span").text
+assert expected == actual,f'expected{expected} did not match actually actual{actual}'
 
-sleep(5)
+if expected and actual:
+    print("SignIn page is successfully opened.")
+else:
+    print("SignIn page did not open correctly.")
+
+sleep(2)
+
+
+
+
+
+
